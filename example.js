3aa23f32d113b7425349afa31b73d0fa44861dcd
Rajashekar <chinoorirajashekarreddy@gmail.com>
2025-03-12 23:54:14 +0530

    Added example.js

---
+++ example.js
@@ -0,0 +1,25 @@
+
+// Git ---> Version control system
+// Repository ---> Codebase
+// Remote repository ---> Centralized codebase hosted on cloud server
+// Local repository ---> A repository on developers machine
+// Git hosting platforms ---> Github, Gitlab etc
+// Branch ---> Separate line of development.
+// Commit ---> Snapshot of changes we made.
+
+
+// Intialization of Git
+// Commit and push changes to remote repository
+
+
+
+
+
+
+console.log("This is from local repo")
+
+
+
+
+
+

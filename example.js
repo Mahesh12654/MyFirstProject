a9cf353172785498b46e6e1fe5e2d9f5c9cf60ee
Mahesh12654 <superbyte6@gmail.com>
2025-03-15 11:18:05 +0530

    Update example.js

--- example.js
+++ example.js
@@ -1,23 +1,11 @@
 
-// Git ---> Version control system
-// Repository ---> Codebase
-// Remote repository ---> Centralized codebase hosted on cloud server
-// Local repository ---> A repository on developers machine
-// Git hosting platforms ---> Github, Gitlab etc
-// Branch ---> Separate line of development.
-// Commit ---> Snapshot of changes we made.
+console.log("This is updated code")
 
+console.log("This is updated code")
 
-// Intialization of Git
-// Commit and push changes to remote repository
+console.log("This is updated code")
 
-
-
-
-
-
-console.log("This is from local repo")
-
+console.log("This is updated code")
 
 
 
